d7fe6183024b37d72a653bbc307d5753b4e2fe8a
bowook <mbosx316@naver.com>
2024-12-03 22:06:18 +0900

    feat(ChristmasDiscount) : 크리스마스 디데이 할인 도메인 추가

--- src/main/java/christmas/domain/ChristmasDDayDiscount.java
+++ src/main/java/christmas/domain/ChristmasDDayDiscount.java
@@ -1,2 +1,22 @@
-package christmas.domain;public class ChristmasDdayDiscount {
+package christmas.domain;
+
+import christmas.domain.constant.Day;
+
+public class ChristmasDDayDiscount {
+    private int discountAmount = 0;
+
+    public int calculate(Day day) {
+        if (!(day.getValue() > Day.DAY_25.getValue())) {
+            discountAmount = 1000 + (day.getValue() - 1) * 100;
+        }
+        return discountAmount;
+    }
+
+    @Override
+    public String toString() {
+        if (discountAmount == 0) {
+            return "";
+        }
+        return String.format("크리스마스 디데이 할인: -%,d" + System.lineSeparator(), discountAmount);
+    }
 }
