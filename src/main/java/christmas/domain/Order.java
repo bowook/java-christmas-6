82ad38dfc974ee35e8d5cfad1c7c56a889fe9a63
bowook <mbosx316@naver.com>
2024-12-03 22:06:42 +0900

    feat(Order) : 주문 도메인 추가

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,2 +1,30 @@
-package christmas.domain;public class Order {
+package christmas.domain;
+
+import christmas.domain.constant.Menu;
+
+public class Order {
+    private final Menu menu;
+    private final int count;
+
+    public Order(final Menu menu, final int count) {
+        this.menu = menu;
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public Menu getMenu() {
+        return menu;
+    }
+
+    public int calculatePrice() {
+        return menu.getPrice() * count;
+    }
+
+    @Override
+    public String toString() {
+        return menu.getName() + " " + count + "개";
+    }
 }
