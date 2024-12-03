10596ae2b7fc795e6b3816ce29419a8a2a8ebede
bowook <mbosx316@naver.com>
2024-12-03 22:10:11 +0900

    feat(ErrorMessage) : 에러 메시지 기능 추가

--- src/main/java/christmas/exception/ErrorMessage.java
+++ src/main/java/christmas/exception/ErrorMessage.java
@@ -1,2 +1,17 @@
-package christmas.exception;public enum ErrorMessage {
+package christmas.exception;
+
+public enum ErrorMessage {
+    DAY_ERROR("유효하지 않은 날짜입니다. 다시 입력해 주세요."),
+    ORDER_ERROR("유효하지 않은 주문입니다. 다시 입력해 주세요.");
+
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
 }
