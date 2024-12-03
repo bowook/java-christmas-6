5294b9bd9469a4c0c1e2ba9f185ad8c82c6edd2f
bowook <mbosx316@naver.com>
2024-12-03 22:09:54 +0900

    feat(CustomException) : 예외 기능 추가

--- src/main/java/christmas/exception/CustomException.java
+++ src/main/java/christmas/exception/CustomException.java
@@ -1,2 +1,13 @@
-package christmas.exception;public class CustomException {
+package christmas.exception;
+
+public class CustomException extends IllegalArgumentException {
+    private static final String PREFIX = "[ERROR] ";
+
+    private CustomException(ErrorMessage errorMessage) {
+        super(PREFIX + errorMessage.getMessage());
+    }
+
+    public static CustomException from(ErrorMessage errorMessage) {
+        return new CustomException(errorMessage);
+    }
 }
