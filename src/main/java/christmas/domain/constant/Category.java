9abf05da9257bae5a8508ad6da4fb27842b51a95
bowook <mbosx316@naver.com>
2024-12-03 22:08:53 +0900

    feat(Category) : 카테고리 도메인 추가

--- src/main/java/christmas/domain/constant/Category.java
+++ src/main/java/christmas/domain/constant/Category.java
@@ -1,2 +1,16 @@
-package christmas.domain;public enum Category {
+package christmas.domain.constant;
+
+public enum Category {
+    APPETIZER("애피타이저"),
+    MAIN("메인"),
+    DESSERT("디저트"),
+    DRINK("음료"),
+    X("없음");
+
+    private final String name;
+
+    Category(final String name) {
+        this.name = name;
+    }
+
 }
