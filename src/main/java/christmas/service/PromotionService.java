1da151e7542b313352ddc386a7cafee621326abd
bowook <mbosx316@naver.com>
2024-12-03 22:10:34 +0900

    feat(PromotionService0 : 할인 서비스 기능 추가

--- src/main/java/christmas/service/PromotionService.java
+++ src/main/java/christmas/service/PromotionService.java
@@ -1,2 +1,72 @@
-package christmas.service;public class PromotionService {
+package christmas.service;
+
+import christmas.domain.ChristmasDDayDiscount;
+import christmas.domain.Orders;
+import christmas.domain.ProvideDiscount;
+import christmas.domain.SpecialDiscount;
+import christmas.domain.WeekDiscount;
+import christmas.domain.constant.Badge;
+import christmas.domain.constant.Day;
+import christmas.domain.constant.Menu;
+
+public class PromotionService {
+    private int totalDiscount = 0;
+    private int expectPayment = 0;
+
+    public Menu provideMenu(Orders orders) {
+        expectPayment = orders.calculateTotalPrice();
+        if (expectPayment >= 120000) {
+            return Menu.CHAMPAGNE;
+        }
+        return Menu.X;
+    }
+
+    public Badge badge() {
+        return Badge.from(totalDiscount);
+    }
+
+    public int expectPayment() {
+        return expectPayment;
+    }
+
+    public int totalDiscount() {
+        return totalDiscount;
+    }
+
+    public String benefitHistory(Day day, Orders orders) {
+        return discountChristmasDDay(day)
+                + discountWeek(day, orders)
+                + discountSpecial(day)
+                + discountProvide(orders);
+    }
+
+    private String discountChristmasDDay(Day day) {
+        ChristmasDDayDiscount christmasDDayDiscount = new ChristmasDDayDiscount();
+        int discount = christmasDDayDiscount.calculate(day);
+        totalDiscount += discount;
+        expectPayment -= discount;
+        return christmasDDayDiscount.toString();
+    }
+
+    private String discountWeek(Day day, Orders orders) {
+        WeekDiscount weekDiscount = new WeekDiscount(day);
+        int discount = weekDiscount.calculate(orders);
+        totalDiscount += discount;
+        expectPayment -= discount;
+        return weekDiscount.toString();
+    }
+
+    private String discountSpecial(Day day) {
+        SpecialDiscount specialDiscount = new SpecialDiscount(day);
+        int discount = specialDiscount.calculateDiscount();
+        totalDiscount += discount;
+        expectPayment -= discount;
+        return specialDiscount.toString();
+    }
+
+    private String discountProvide(Orders orders) {
+        ProvideDiscount provideDiscount = new ProvideDiscount();
+        totalDiscount += provideDiscount.calculate(orders);
+        return provideDiscount.toString();
+    }
 }
