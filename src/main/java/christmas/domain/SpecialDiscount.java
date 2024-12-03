9871bc536883777ee3d5bf1088ce9d76cf2b3fe6
bowook <mbosx316@naver.com>
2024-12-03 22:07:48 +0900

    feat(SpecialDiscount) : 특별 할인 도메인 추가

--- src/main/java/christmas/domain/SpecialDiscount.java
+++ src/main/java/christmas/domain/SpecialDiscount.java
@@ -1,2 +1,27 @@
-package christmas.domain;public class SpecialDiscount {
+package christmas.domain;
+
+import christmas.domain.constant.Day;
+
+public class SpecialDiscount {
+    private int discountAmount = 0;
+    private final Day day;
+
+    public SpecialDiscount(final Day day) {
+        this.day = day;
+    }
+
+    public int calculateDiscount() {
+        if (day.isStar()) {
+            discountAmount = 1000;
+        }
+        return discountAmount;
+    }
+
+    @Override
+    public String toString() {
+        if (discountAmount == 0) {
+            return "";
+        }
+        return String.format("특별 할인: -%,d원" + System.lineSeparator(), discountAmount);
+    }
 }
