3e9cddbb5aca65665457b65a7eb32a6027c97b34
bowook <mbosx316@naver.com>
2024-12-03 22:08:15 +0900

    feat(WeekDiscount) : 평일, 주말 할인 도메인 추가

--- src/main/java/christmas/domain/WeekDiscount.java
+++ src/main/java/christmas/domain/WeekDiscount.java
@@ -1,2 +1,31 @@
-package christmas.domain;public class WeekDiscount {
+package christmas.domain;
+
+import christmas.domain.constant.Category;
+import christmas.domain.constant.Day;
+
+public class WeekDiscount {
+    private final static int discountStandard = 2023;
+    private int discountAmount = 0;
+    private final Day day;
+
+    public WeekDiscount(Day day) {
+        this.day = day;
+    }
+
+    public int calculate(Orders orders) {
+        if (day.getWeek().equals("평일")) {
+            discountAmount += orders.findMenu(Category.DESSERT) * discountStandard;
+            return discountAmount;
+        }
+        discountAmount += orders.findMenu(Category.MAIN) * discountStandard;
+        return discountAmount;
+    }
+
+    @Override
+    public String toString() {
+        if (discountAmount == 0) {
+            return "";
+        }
+        return String.format("%s 할인: -%,d원" + System.lineSeparator(), day.getWeek(), discountAmount);
+    }
 }
