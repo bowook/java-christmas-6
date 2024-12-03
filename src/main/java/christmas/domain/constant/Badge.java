da3bbe67e7c28d9d314966008f735a4933b483fc
bowook <mbosx316@naver.com>
2024-12-03 22:08:34 +0900

    feat(Badge) : 배지 도메인 추가

--- src/main/java/christmas/domain/constant/Badge.java
+++ src/main/java/christmas/domain/constant/Badge.java
@@ -1,2 +1,31 @@
-package christmas.domain.constant;public enum Badge {
+package christmas.domain.constant;
+
+public enum Badge {
+    STAR("별"),
+    TREE("트리"),
+    SANTA("산타"),
+    X("없음");
+
+    private final String name;
+
+    Badge(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static Badge from(int price) {
+        if (price >= 20000) {
+            return SANTA;
+        }
+        if (price >= 10000) {
+            return TREE;
+        }
+        if (price >= 5000) {
+            return STAR;
+        }
+        return X;
+    }
 }
