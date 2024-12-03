32d4adde81a39cab63b8559d3e037bb759b1e91c
bowook <mbosx316@naver.com>
2024-12-03 22:07:27 +0900

    feat(ProvideDiscount) : 증정 이벤트 도메인 추가

--- src/main/java/christmas/domain/ProvideDiscount.java
+++ src/main/java/christmas/domain/ProvideDiscount.java
@@ -1,2 +1,23 @@
-package christmas.domain;public class ProvideDiscount {
+package christmas.domain;
+
+import christmas.domain.constant.Menu;
+
+public class ProvideDiscount {
+    private int discountAmount = 0;
+
+    public int calculate(Orders orders) {
+        int beforeDiscountTotalAmount = orders.calculateTotalPrice();
+        if (beforeDiscountTotalAmount >= 120000) {
+            discountAmount = Menu.CHAMPAGNE.getPrice();
+        }
+        return discountAmount;
+    }
+
+    @Override
+    public String toString() {
+        if (discountAmount == 0) {
+            return "";
+        }
+        return String.format("증정 이벤트: -%,d원" + System.lineSeparator(), discountAmount);
+    }
 }
